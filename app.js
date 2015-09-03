38aec1772075df21465d5d0143d484c4e7998e7d
sebastiangima <sebastiangima@gmail.com>
2015-09-03 15:09:14 -0300

    modify app
    to run in openshift web site

--- app.js
+++ app.js
@@ -1,51 +1,204 @@
-var express = require("express"), 
-http = require("http"),
-app = express(),
-server = http.createServer(app,'0.0.0.0'),
-path = require('path'),
- fs = require('fs');
+#!/bin/env node
+//  OpenShift sample Node application
+var express = require('express');
+var fs      = require('fs');
+var http = require("http");
+var path = require('path');
 
 
-var unidos = {}, rooms={}, chesses={jugadas:[]}, inRooms={};
+var usuariosOnline = {}, unidos = {}, rooms={}, chesses={jugadas:[]}, inRooms={};
 
 
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.set("views",__dirname + "/views");
-app.configure(function(){
-	app.use(express.static(__dirname));
-});
+/**
+ *  Define the sample application.
+ */
+var SampleApp = function() {
 
-app.get("/save", function(req,res){
-	console.log(req);
-	res.render("index2.jade", {title : "Server functions"});
-});
-app.get("/", function(req,res){
-	
-	res.render("index.jade", {title : "Chat con NodeJS, Express, Socket.IO y jQuery"});
-});
-var usuariosOnline = {};
+    //  Scope.
+    var self = this;
 
 
-app.get("/jugadores", function(req,res){
-	var result=''
-  for (var i in unidos) {
-    if (result) result+=',';
-    result += unidos[i]+','+i
-  }
-  
-  var body = JSON.stringify(unidos);
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end(body);  
+    /*  ================================================================  */
+    /*  Helper functions.                                                 */
+    /*  ================================================================  */
 
-  
-});
+    /**
+     *  Set up server IP address and port # using env variables/defaults.
+     */
+    self.setupVariables = function() {
+        //  Set the environment variables we need.
+        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8443;
+   //     self.port      = 8443;
+
+        if (typeof self.ipaddress === "undefined") {
+            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
+            //  allows us to run/test the app locally.
+            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
+            self.ipaddress = "127.0.0.1";
+        };
+    };
 
 
-server.listen(8080,'0.0.0.0');
+    /**
+     *  Populate the cache.
+     */
+    self.populateCache = function() {
+        if (typeof self.zcache === "undefined") {
+            self.zcache = { 'index.html': '' };
+        }
 
-//objecto para guardar en la sesión del socket a los que se vayan conectando
+        //  Local cache for static content.
+        self.zcache['index.html'] = fs.readFileSync('./index.html');
+    };
 
+
+    /**
+     *  Retrieve entry (content) from cache.
+     *  @param {string} key  Key identifying content to retrieve from cache.
+     */
+    self.cache_get = function(key) { return self.zcache[key]; };
+
+
+    /**
+     *  terminator === the termination handler
+     *  Terminate server on receipt of the specified signal.
+     *  @param {string} sig  Signal to terminate on.
+     */
+    self.terminator = function(sig){
+        if (typeof sig === "string") {
+           console.log('%s: Received %s - terminating sample app ...',
+                       Date(Date.now()), sig);
+           process.exit(1);
+        }
+        console.log('%s: Node server stopped.', Date(Date.now()) );
+    };
+
+
+    /**
+     *  Setup termination handlers (for exit and a list of signals).
+     */
+    self.setupTerminationHandlers = function(){
+        //  Process on exit and signals.
+        process.on('exit', function() { self.terminator(); });
+
+        // Removed 'SIGPIPE' from the list - bugz 852598.
+        ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
+         'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
+        ].forEach(function(element, index, array) {
+            process.on(element, function() { self.terminator(element); });
+        });
+    };
+
+
+    /*  ================================================================  */
+    /*  App server functions (main app logic here).                       */
+    /*  ================================================================  */
+
+    /**
+     *  Create the routing table entries + handlers for the application.
+     */
+    self.createRoutes = function() {
+        self.routes = { };
+				
+				
+				self.app.use(express.static(path.join(__dirname, 'public')));
+
+				self.app.set("views",__dirname + "/views");
+				self.app.configure(function(){
+					self.app.use(express.static(__dirname));
+				});
+
+				self.routes['/save']= function(req,res){
+					console.log(req);
+					res.render("index2.jade", {title : "Server functions"});
+				};
+				self.routes['/']= function(req,res){
+					res.render("index.jade", {title : "Chat con NodeJS, Express, Socket.IO y jQuery"});
+				};
+				self.routes['/jugadores']= function(req,res){
+					var result=''
+					for (var i in unidos) {
+						if (result) result+=',';
+						result += unidos[i]+','+i
+					}
+					
+					var body = JSON.stringify(unidos);
+					res.writeHead(200, {'Content-Type': 'text/html'});
+					res.end(body);  
+
+					
+				};
+
+        self.routes['/asciimo'] = function(req, res) {
+            var link = "http://i.imgur.com/kmbjB.png";
+            res.send("<html><body><img src='" + link + "'></body></html>");
+        };
+
+        // self.routes['/'] = function(req, res) {
+            // res.setHeader('Content-Type', 'text/html');
+            // res.send(self.cache_get('index.html') );
+        // };
+    };
+
+		self.initializeSocket = function() {
+			
+		}
+
+    /**
+     *  Initialize the server (express) and create the routes and register
+     *  the handlers.
+     */
+    self.initializeServer = function() {
+
+        self.app = express();
+				self.server=http.createServer(self.app);
+        self.createRoutes();
+        //  Add handlers for the app (from the routes).
+        for (var r in self.routes) {
+            self.app.get(r, self.routes[r]);
+        }
+    };
+
+
+    /**
+     *  Initializes the sample application.
+     */
+    self.initialize = function() {
+        self.setupVariables();
+        // self.populateCache();
+        self.setupTerminationHandlers();
+
+        // Create the express server and routes.
+        self.initializeServer();
+        self.initializeSocket();
+    };
+
+
+    /**
+     *  Start the server (starts up the sample application).
+     */
+    self.start = function() {
+        //  Start the app on the specific interface (and port).
+        // self.app.listen(self.port, self.ipaddress, function() {
+        self.server.listen(self.port, self.ipaddress, function() {
+            console.log('%s: Node server started on %s:%d ...',
+                        Date(Date.now() ), self.ipaddress, self.port);
+        });
+    };
+
+		return this;
+};   /*  Sample Application.  */
+
+
+/**
+ *  main():  Main code.
+ */
+var zapp = new SampleApp();
+zapp.initialize();
+var io = require("socket.io").listen(zapp.server);
+zapp.start();
 
 function onready(){
 console.log('ready')
@@ -53,9 +206,13 @@ console.log('ready')
 }
 
 
-var io = require("socket.io").listen(server);
+
+//objecto para guardar en la sesión del socket a los que se vayan conectando
+
+
+
 onready()
-//al conectar un usuario||socket, este evento viene predefinido por socketio
+
 io.sockets.on('connection', function(socket) 
 {
 	//cuando el usuario conecta al chat comprobamos si está logueado
